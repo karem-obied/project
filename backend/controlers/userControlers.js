ebdd60736fa7f72f0ae39932311e0b4f6868314c
karem-obied <karemobied1@gmail.com>
2022-12-23 10:45:03 +0300

    making userControl , Adding MongoURI , adding .env

---
+++ backend/controlers/userControlers.js
@@ -0,0 +1,70 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../model/User");
+
+const reg = asyncHandler(async (req, res) => {
+  const { userName, email, password } = req.body;
+  if (userName && email && password) {
+    const exist = await User.findOne({ email });
+    if (exist) {
+      res.status(400);
+      throw new Error("This Email Is Used Before This Time Try Another One");
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, salt);
+      const user = await User.create({
+        userName,
+        email,
+        password: hash,
+      });
+      res.status(200).json({
+        _id: user._id,
+        userName,
+        email,
+        token: generate(user._id),
+      });
+    }
+  } else {
+    res.status(400);
+    throw new Error("Please Fill All The Fields");
+  }
+});
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    if (email && (await bcrypt.compare(password, user.password))) {
+      res.status(200).json({
+        _id: user._id,
+        userName,
+        email,
+        token: generate(user._id),
+      });
+    } else {
+      if (!email) {
+        res.status(400);
+        throw new Error("Please Put Your Email");
+      } else {
+        res.status(400);
+        throw new Error("Your Password Is Worng");
+      }
+    }
+  } else {
+    res.status(400);
+    throw new Error("This Account Does not Exist");
+  }
+});
+const logout = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    res.status(400);
+    throw new Error("User Does Not Exist");
+  } else {
+    await user.remove();
+    res.status(200).json({ id: user.params.id });
+  }
+});
+const generate = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
--- backend/model/User.js
+++ backend/model/User.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
-const UserSchema = mongoose.Schema({
-  userName: {
-    type: String,
-    required: [true, "Please Set Your User Name"],
+const UserSchema = mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "Please Set Your User Name"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please Set Your User Name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please Set Your User Name"],
+    },
   },
-  password: {
-    type: String,
-    required: [true, "Please Set Your User Name"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please Set Your User Name"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 module.exports = mongoose.model("User", UserSchema);
