0fe1080e61be18bca273b7fec7d81e564ad319d4
karem-obied <karemobied1@gmail.com>
2023-01-01 23:13:20 +0300

    Complete Header

--- frontend/JS/main.js
+++ frontend/JS/main.js
@@ -1,1 +1,28 @@
-console.log(Date.now());
+document.body.onload = () => {
+  const gear = document.querySelector(".options");
+  const hideList = document.querySelector(".options ul");
+
+  gear.addEventListener("click", () => {
+    if (gear.classList.contains("active")) {
+      gear.classList.remove("active");
+      hideList.classList.add("hide");
+      hideList.classList.remove("show");
+    } else {
+      gear.classList.add("active");
+      hideList.classList.remove("hide");
+      hideList.classList.add("show");
+    }
+  });
+  document.addEventListener("click", (e) => {
+    if (
+      !(
+        (e.target.localName == "ul" && e.target.classList.contains("hide")) ||
+        (e.target.localName == "svg" && e.target.classList.contains("ul"))
+      )
+    ) {
+      gear.classList.remove("active");
+      hideList.classList.add("hide");
+      hideList.classList.remove("show");
+    }
+  });
+};
